6e9796e5e87f2ca11a937a148752007334ca8978
Joey Buczek <joe.buczek@gmail.com>
2016-02-17 20:46:25 -0500

    "udpated readme and home html"

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 Free Code Camp Challenge
 
+This service will allow you to search for images, and will return (in JSON format) the URLs, Alt Text, and Page Urls for each image found.
+
+To perform a new search, append the following criteria to the `/api/imagesearch` path:
+
+    - `searchfor=<yourSearchCriteria>`
+    
+    - `offset=<pageNumberOfResults>`
+    
+** Example Searches **
+
+`/api/imagesearch?searchfor=iceland&offset=1`
+
+`/api/imagesearch?searchfor=reykjavik&offset=5`
+
+To see the latest image searches, use the following path: `/api/latest/imagesearch`. The response will be a JSON object.
--- routes.js
+++ routes.js
@@ -24,7 +24,17 @@ router.get(/^\/api\/latest\/imagesearch+/i, function (req, res) {
 
 
 // html templates
-var htmlHome = "<h2>Image Search Tool</h2>";
+var htmlHome = "<h2>Image Search Tool</h2>" +
+               "<p>This service will allow you to search for images, and will return (in JSON format) the URLs, " + 
+               "Alt Text, and Page URLS for each image found.</p>" +
+               "<p>To perform a new search, append the following criteria to the <span style='color:royalblue;'>/api/imagesearch</span> path:</p>" +
+               "<pre style='color:royalblue;'>searchfor=&lt;yourSearchCriteria&gt;</pre>" +
+               "<pre style='color:royalblue;'>offset=&lt;pageNumberOfResults&gt;</pre>" +
+               "<b>Example Searches:</b>" +
+               "<pre style='color:royalblue;'>/api/imagesearch?searchfor=iceland&offset=1</pre>" + 
+               "<pre style='color:royalblue;'>/api/imagesearch?searchfor=reykjavik&offset=5</pre>" +
+               "<p>To see the latest image searches, use the following path: <span style='color:royalblue;'>/api/latest/imagesearch</span>. " + 
+               "The response will be a JSON object.</p>";
 
 // export
 module.exports = router;
