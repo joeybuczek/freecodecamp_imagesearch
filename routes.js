36b6973a2b7efada6cfc10e6ee781a267ed35528
Joey Buczek <joe.buczek@gmail.com>
2016-02-20 01:42:20 -0500

    "added bing image search api"

--- routes.js
+++ routes.js
@@ -4,21 +4,40 @@
 var express = require('express');
 var router = express.Router();
 var mongoFn = require('./mongoFn');
+var Search = require('bing.search');
+
+// env vars
+if (!process.env.API_KEY) { require('./env'); }
+
+// bing image search api vars
+var imageSearch = new Search(process.env.API_KEY);
 
 // home page
 router.get('/', function (req, res) {
     res.send(htmlHome);
 });
 
-// 2-19-16 need incoming search route - imgur api?
+// incoming search route - bing api
 router.get(/^\/api\/imagesearch+/i, function (req, res) {
     
     var searchFor = req.param('searchfor');
     var page = req.param('offset') || 1;
-    res.send("You're searching for: " + searchFor + " with this many pages: " + page);
     
-    // perform db cleanup
-    // mongoFn.databaseCleanup();
+    imageSearch.images(searchFor, { 'skip' : page - 1 }, function (err, results) {
+        // store and format only 10 of the results
+        var displayArray = [];
+        for (var i = 0; i < 10; i++) {
+            displayArray.push( { 'title':results[i].title, 'url':results[i].url, 'sourceUrl':results[i].sourceUrl } );
+        }
+        // display json formatted results
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(displayArray, null, 4));
+    });
+    
+    // record search and cleanup db afterwards
+    mongoFn.insertSearch(searchFor, function(result) {
+        if (result) mongoFn.databaseCleanup();
+    });
     
 });
 
@@ -34,14 +53,6 @@ router.get(/^\/api\/latest\/imagesearch+/i, function (req, res) {
     mongoFn.databaseCleanup();
     
 });
-
-// 2-19-16 testing mongo insert - this will eventually be functionality for new image search
-router.get(/^\/test+/i, function(req, res){
-    mongoFn.insertSearch("iceland test", function(result){
-        res.send('The result of the insert is: ' + result);
-    });
-});
-
 
 // html templates
 var htmlHome = "<h2>Image Search Tool</h2>" +
